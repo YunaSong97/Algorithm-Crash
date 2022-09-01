65b933d457d1650305bbda4511b20876ed72b683
yunasong <33195517+YunaSong97@users.noreply.github.com>
2022-09-01 15:57:36 +0900

    Update Main_BOJ_2470.java

--- BOJ/2470_두_용액/송유나/Main_BOJ_2470.java
+++ BOJ/2470_두_용액/송유나/Main_BOJ_2470.java
@@ -3,7 +3,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main_BOJ_2470 {
